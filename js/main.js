178c79f1a150950d8ae17315fe78f654f2a42709
Senseytreyser <senseytreyser@mail.ru>
2019-05-29 23:53:29 +0300

    Атрибуты и списки

---
+++ js/main.js
@@ -0,0 +1,15 @@
+﻿var app = new Vue({
+    el: "#app",
+    data: {
+        product:"Носки",
+        image: "https://avatars.mds.yandex.net/get-marketpic/239743/market_CHeoFtQoQPyfkF3F2ieYJQ/orig",
+        description: "Картинка с носками",
+        height: "200",
+        inState: false,
+        prop: ['удобные','хлопковые','лёгкие'],
+        variants: [
+            { id: 1234, color: 'чёрный' },
+            { id: 1233, color: 'белый' }
+        ] 
+    }
+})
