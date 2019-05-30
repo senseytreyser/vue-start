3e671197d36aa7ed6c530bba759d726648b3508a
Senseytreyser <senseytreyser@mail.ru>
2019-05-31 00:40:46 +0300

    Работа со стилями

--- js/main.js
+++ js/main.js
@@ -2,14 +2,29 @@
     el: "#app",
     data: {
         product:"Носки",
+        brand: "Ивановские",
         image: "https://avatars.mds.yandex.net/get-marketpic/239743/market_CHeoFtQoQPyfkF3F2ieYJQ/orig",
         description: "Картинка с носками",
         height: "200",
-        inState: false,
+        inState: true,
         prop: ['удобные','хлопковые','лёгкие'],
         variants: [
-            { id: 1234, color: 'чёрный' },
-            { id: 1233, color: 'белый' }
-        ] 
+            { id: 1234, color: 'black', img: 'https://avatars.mds.yandex.net/get-marketpic/239743/market_CHeoFtQoQPyfkF3F2ieYJQ/orig' },
+            { id: 1233, color: 'gray', img: 'https://avatars.mds.yandex.net/get-marketpic/196766/market_duo2OqLZX8r3JIYAoxXigQ/orig' }
+        ],
+        card: 0
+    },
+    methods: {
+        addToCard(){
+            this.card++;
+        },
+        changeProduct(imgUrl){
+            this.image = imgUrl;
+        }
+    },
+    computed: {
+        title(){
+            return this.brand + " " + this.product;
+        }
     }
 })
